9b81423aa9cbb6b119877abf980f0a0d3a14c555
Aaron F Stanton <afstanton@gmail.com>
2015-06-18 10:04:34 -0400

    Add gitattributes to recipe.

--- app-template.rb
+++ app-template.rb
@@ -59,6 +59,26 @@ end
 
 run 'bundle install'
 
+file '.gitattributes', <<-CODE
+# Auto detect text files and perform LF normalization
+* text=auto
+
+# Custom for Visual Studio
+*.cs     diff=csharp
+
+# Standard to msysgit
+*.doc    diff=astextplain
+*.DOC    diff=astextplain
+*.docx diff=astextplain
+*.DOCX diff=astextplain
+*.dot  diff=astextplain
+*.DOT  diff=astextplain
+*.pdf  diff=astextplain
+*.PDF    diff=astextplain
+*.rtf    diff=astextplain
+*.RTF    diff=astextplain
+CODE
+
 generate 'simple_form:install --bootstrap'
 
 generate 'rspec:install'
